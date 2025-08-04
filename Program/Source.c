49f507a564eda94972817609d065942c0c23f746
changgyu359 <gchl8548@gmail.com>
2025-08-04 19:47:05 +0900

    (2025.8.4) 메모리 영역

--- Program/Source.c
+++ Program/Source.c
@@ -1,111 +1,36 @@
 ﻿#include<stdio.h>
 
-void initialize()
+int main()
 {
-	printf("initialize...\n");
-}
+#pragma region 메모리 영역
 
-int max_health()
-{
-	
-	return 10;
-}
-
-void criticalize(int x)
-{
-	printf("크리티컬! 2배피해! %d\n", 2*x);
-}
-
-void print_status(char grade, int height, float temperature)
-{
-	printf("등급 : %c\n", grade);
-	printf("키 : %d\n", height);
-	printf("체온 : %f\n", temperature);
-}
-
-void swap(int* a, int* b)
-{
-	int temp=*a;
-	*a = *b;
-	*b = temp;
-
-}
-
-void find(int count)
-{if(count<=0)
-{	
-	printf("find fail.\n");
-	return;
-}
-
-	printf("%d\n",count);
-	
-	find(count-1);
-}
-
-
-void main()
-{
-#pragma region 함수
-	// 하나의 특별한 목적의 작업을 수행하기 위해 독립적으로 설계된 코드의 집합.
-
-	//initialize();
-#pragma endregion
-
-#pragma region 반환형
-	// 함수가 실행을 끝내고 돌려주는 결과의 자료형.
-	// 함수의 경우 자료형과 반환하는 값의 형태가 일치하지 않으면 원하는 값을 얻을 수 없음.
-
-	//printf("최대 체력: %d\n",max_health());
-
-#pragma endregion
-
-#pragma region 매개 변수
-	// 함수의 정의에서 전달받은 인수를 함수 내부로 전달하기 위해 사용하는 변수.
-
-	//criticalize(20);
-
-	// 매개 변수는 함수 내부에서만 연산이 이루어지며, 함수가 종료되면 메모리에서 해제됨.
-
-	// 하나의 함수에 서로 다른 자료형의 매개 변수를 함께 정의할 수 있으며,
-	// 여러 개의 매개 변수를 생성할 수 있음.
-
-	//print_status('A', 160, 36.5);
-#pragma endregion
-
-#pragma region 인수
-	// 함수가 호출될 때 매개 변수에 실제로 전달되는 값.
-
-	// 인수의 경우 함수에 있는 매개 변수에 따라 전달할 수 있는 인수의 수가 결정되며,
-	// 값을 전달하는 인수와 값을 전달받는 매개 변수의 자료형이 서로 일치해야함.
-
-
-
-	//int x1 = 10;
-	//int y1 = 20;
-
-	//swap(&x1, &y1);
-
-	//
-
-	//printf("실제값\n");
-	//printf("x1=%d,y1=%d\n", x1, y1);
-
-	
-
-#pragma endregion
-
-#pragma region 재귀 함수
-	// 어떤 함수에서 자신을 다시 호출하여 작업을 수행하는 함수.
-
-	// 재귀 함수는 함수를 계속 호출하기 때문에 
-	// 스택 영역에 메모리가 계속 쌓이게 되므로 스택 오버플로우가 발생함.
-
-
-	find(3);
+#pragma region Code 영역
+	// 프로그램을 실행하기 위해 필요한 코드가 저장되는 영역으로
+	// 함수의 주소와 상수가 저장되는 메모리 영역.
 
 
 #pragma endregion
 
+#pragma region Data 영역
+	// 프로그램의 시작과 함께 메모리에 저장되며, 프로그램이 종료될 때 메모리가 해제되는 영역.
+#pragma endregion
 
+#pragma region BSS 영역
+	// 프로그램이 실행될 때 초기화가 이루어지지 않은 전역 변수와 정적 변수가 저장되는 메모리 영역.
+#pragma endregion
+
+#pragma region Stack 영역
+	// 프로그램이 자동으로 사용하는 임시 메모리 영역으로 함수 호출 시
+	// 생성되는 지역 변수와 매개 변수가 저장되는 메모리 영역.
+#pragma endregion
+
+#pragma region Heap 영역
+	// 사용자가 직접 메모리 공간을 할당해주는 메모리 영역이며, 
+	// 사용하지 않는 경우 사용자가 직접 메모리를 헤제해야 되는 메모리 영역.
+#pragma endregion
+
+
+#pragma endregion
+
+	return 0;
 }
