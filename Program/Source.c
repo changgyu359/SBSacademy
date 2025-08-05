c847d03c7ade688ab71c44b0fe7f1d27f4f9fa9d
changgyu359 <gchl8548@gmail.com>
2025-08-05 19:45:27 +0900

    (2025.8.5) 동적 할당

--- Program/Source.c
+++ Program/Source.c
@@ -1,36 +1,44 @@
 ﻿#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
 
 int main()
 {
-#pragma region 메모리 영역
+#pragma region 동적 할당
+	// 프로그램을 실행하는 중에 필요한 만큼 메모리를 할당하는 작업.
 
-#pragma region Code 영역
-	// 프로그램을 실행하기 위해 필요한 코드가 저장되는 영역으로
-	// 함수의 주소와 상수가 저장되는 메모리 영역.
+	int* pointer = (int*) malloc(4);
 
+	*pointer = 10;
+
+	printf("pointer가 가리키는 값:%d\n", *pointer);
+	
+	// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를 변경할 수 있으며,
+	// 동적으로 메모리의 크기를 할당할 때 바이트 단위로 지정함.
+	
+	free(pointer);
+	// 동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로,
+	// 사용이 끝나면 직접 해제해주어야 함.
+
+
+	// 과제 동적배열 만들기
+	// 1.int를 사용해 3개짜리 배열을 만든다.
+	// 2.pointer를 동적배열에 연결
+	// 3.각 배열에 차례로 10,20,30을 넣는다.
+	// 4.출력 해보고 포인터 해제
+
+	pointer = malloc(3);
+
+	for (int i = 0, i < 3; i++)
+	{
+
+	}
 
 #pragma endregion
 
-#pragma region Data 영역
-	// 프로그램의 시작과 함께 메모리에 저장되며, 프로그램이 종료될 때 메모리가 해제되는 영역.
-#pragma endregion
-
-#pragma region BSS 영역
-	// 프로그램이 실행될 때 초기화가 이루어지지 않은 전역 변수와 정적 변수가 저장되는 메모리 영역.
-#pragma endregion
-
-#pragma region Stack 영역
-	// 프로그램이 자동으로 사용하는 임시 메모리 영역으로 함수 호출 시
-	// 생성되는 지역 변수와 매개 변수가 저장되는 메모리 영역.
-#pragma endregion
-
-#pragma region Heap 영역
-	// 사용자가 직접 메모리 공간을 할당해주는 메모리 영역이며, 
-	// 사용하지 않는 경우 사용자가 직접 메모리를 헤제해야 되는 메모리 영역.
-#pragma endregion
-
-
-#pragma endregion
 
 	return 0;
 }
